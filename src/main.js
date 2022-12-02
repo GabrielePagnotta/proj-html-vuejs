2e408269c146182bc09d9368c59e6bc78d9657cb
GabrielePagnotta <gabriele.pagnotta1998@gmail.com>
2022-12-02 12:26:15 +0100

    inizializzazione progetto finale front end

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import {  } from '@fortawesome/free-solid-svg-icons'
+import {  } from '@fortawesome/free-regular-svg-icons'
+/* add icons to the library */
+library.add()
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
 Vue.config.productionTip = false
 
 new Vue({
